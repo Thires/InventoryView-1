cd6dae4eb968093f007cfa81e38118f4caaba8f2
Thires <thiresdr@gmail.com>
2025-04-15 22:00:16 -0400

    Version 3.0c

---
+++ Cases/CaseInventory.cs
@@ -0,0 +1,66 @@
+﻿namespace InventoryView.Cases
+{
+    public class CaseInventory
+    {
+        private readonly plugin plugin;
+
+        public CaseInventory(plugin pluginInstance)
+        {
+            plugin = pluginInstance;
+        }
+
+        public void InventoryCase(string trimtext, string originalText, ref string scanMode, ref int level, ref ItemData lastItem, CharacterData currentData)
+        {
+            if (trimtext.StartsWith("Roundtime:"))
+            {
+                plugin.PauseForRoundtime(trimtext);
+                InventoryEnd(ref scanMode);
+                return;
+            }
+
+            if (trimtext.StartsWith("[Use"))
+                return;
+
+            InventoryItems(trimtext, originalText, ref level, ref lastItem, currentData);
+        }
+
+        private static void InventoryEnd(ref string scanMode)
+        {
+            plugin.Host.EchoText("Checking Pocket");
+            plugin.Host.SendText("tap pocket");
+            scanMode = "PocketStart";
+        }
+
+
+        private static void InventoryItems(string trimtext, string originalText, ref int level, ref ItemData lastItem, CharacterData currentData)
+        {
+            int spaces = originalText.Length - originalText.TrimStart().Length;
+            int newlevel = (spaces + 1) / 3;
+
+            string tap = plugin.CleanTapText(trimtext);
+
+            if (newlevel == 1)
+            {
+                lastItem = currentData.AddItem(new ItemData { tap = tap });
+            }
+            else if (newlevel == level)
+            {
+                lastItem = lastItem.parent.AddItem(new ItemData { tap = tap });
+            }
+            else if (newlevel == level + 1)
+            {
+                lastItem = lastItem.AddItem(new ItemData { tap = tap });
+            }
+            else
+            {
+                for (int i = newlevel; i <= level; i++)
+                {
+                    lastItem = lastItem.parent;
+                }
+                lastItem = lastItem.AddItem(new ItemData { tap = tap });
+            }
+
+            level = newlevel;
+        }
+    }
+}
---
+++ Cases/CaseMoonMage.cs
@@ -0,0 +1,84 @@
+﻿using System.Text.RegularExpressions;
+
+namespace InventoryView.Cases
+{
+    public class CaseMoonMage
+    {
+        private readonly plugin plugin;
+
+        public CaseMoonMage(plugin pluginInstance)
+        {
+            plugin = pluginInstance;
+        }
+
+        public void MoonMageCase(string trimtext, string fullText, ref string scanMode, ref int level, ref ItemData lastItem, CharacterData currentData)
+        {
+            if (scanMode == "MoonMageStart")
+            {
+                if (Regex.IsMatch(trimtext, @"^You feel fully prepared to cast your spell\."))
+                {
+                    plugin.Host.EchoText("Scanning Shadow Servant.");
+                    plugin.Host.SendText("cast servant");
+                    return;
+                }
+
+                if (trimtext == "Within the belly of the Shadow Servant you see:")
+                {
+                    plugin.ScanStart("MoonMage");
+                    return;
+                }
+
+                if (plugin.IsDenied(trimtext))
+                {
+                    scanMode = null;
+                    plugin.Host.EchoText("Skipping Servant, Piercing Gaze required.");
+                    plugin.Host.EchoText("Scan Complete.");
+                    plugin.Host.SendText("#parse Scan Complete");
+                    LoadSave.SaveSettings();
+                    return;
+                }
+
+                return;
+            }
+
+            if (scanMode == "MoonMage")
+            {
+                if (fullText.StartsWith("Your Servant is holding"))
+                {
+                    scanMode = null;
+                    plugin.Host.EchoText("Scan Complete.");
+                    plugin.Host.SendText("#parse Scan Complete");
+                    LoadSave.SaveSettings();
+                    return;
+                }
+
+                int spaces = fullText.Length - fullText.TrimStart().Length;
+                spaces = spaces >= 4 && spaces <= 14 && spaces % 2 == 0 ? spaces / 2 - 1 : spaces == 15 ? 7 : 1;
+
+                string tap = Regex.Replace(trimtext, @"^(an?|some|several)\s", "");
+
+                if (spaces == 1)
+                {
+                    lastItem = currentData.AddItem(new ItemData { tap = tap });
+                }
+                else if (spaces == level)
+                {
+                    lastItem = lastItem.parent.AddItem(new ItemData { tap = tap });
+                }
+                else if (spaces == level + 1)
+                {
+                    lastItem = lastItem.AddItem(new ItemData { tap = tap });
+                }
+                else
+                {
+                    for (int i = spaces; i <= level; i++)
+                        lastItem = lastItem.parent;
+
+                    lastItem = lastItem.AddItem(new ItemData { tap = tap });
+                }
+
+                level = spaces;
+            }
+        }
+    }
+}
